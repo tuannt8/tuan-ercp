3988c291a85aadbefa4726b7007404af55d31688
nguyentrungtuan.107@gmail.com <nguyentrungtuan.107@gmail.com@387d5d86-a975-01e3-4a56-1d5cb435c38f>
2012-12-10 07:32:03 +0000

    Major papilla model test 1

--- trunk/ERCP_contact_cut/include/DataTypes/define.h
+++ trunk/ERCP_contact_cut/include/DataTypes/define.h
@@ -22,7 +22,7 @@
 #define E		1000
 #define NU		0.3
 #define DENSITY	0.01 //0.02 for majorpapilla; 0.001 for liver; 0.1 for papilla
-#define DAMPING	100
+#define DAMPING	1000
 
 #define TOOL_RADIUS 3
 
